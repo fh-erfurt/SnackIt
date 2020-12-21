90d0a8efacc1ac8ff061febb4f3079ff17c6a2e2
Thomas Konietzny <thomas.konietzny@fh-erfurt.de>
2020-12-21 11:30:33 +0100

    added Database Init

---
+++ project/init/10_database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$dns = 'mysql:host=localhost;dbname=SnackIt';
+$dbuser = 'root';
+$dbpassword = '';
+$options    = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+
+$db = null;
+
+try
+{
+	$db = new PDO($dns, $dbuser, $dbpassword, $options);
+}
+catch(\PDOException $e)
+{
+	die( 'Database connection failed: ' . $e->getMessage() );
+}
+?>
