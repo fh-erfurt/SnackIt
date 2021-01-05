eea75567325a635f92aba16941c0677d2c44e679
Mrvn_ <marvin.hof@fh-erfurt.de>
2021-01-05 17:38:43 +0100

    added login.php

---
+++ project/views/login.php
@@ -0,0 +1,9 @@
+<form method='post' name='loginForm' id=login-form>
+	<label for='email'>E-Mail</label><br/>
+	<input type='email' name='email' id='email'>
+	<span id='emailError'></span><br/>
+	<label for='password'>Passwort</label><br/>
+	<input type='password' name='password' id='password'/><br/>
+	<span id='passwordError'></span><br />
+	<input type='submit' name='submitBtn' value='Anmelden' class='loginButton'><br/>
+</form>
