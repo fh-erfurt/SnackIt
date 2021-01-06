058964df297b7b4c482ced9891c160ee0b502ae5
Lukas491don <Jan204@t-online.de>
2021-01-06 16:46:58 +0100

    added classes in login.css

--- project/views/login.php
+++ project/views/login.php
@@ -1,9 +1,27 @@
+<link rel='stylesheet' type='text/css' href='assets/css/login.css'>
+
+<h1>Login</h1>
+<section  class=loginstyle>
+<? if(isset($error)): ?>
+	<p class='errorMessage' id=phpError><?=$error?></p>
+<? endif;?>
+
+
+
+<br />
+<br />
+<br />
+<br />
+
+
 <form method='post' name='loginForm' id=login-form>
-	<label for='email'>E-Mail</label><br/>
+	<label for='email'>E-Mail</label> <br />
 	<input type='email' name='email' id='email'>
-	<span id='emailError'></span><br/>
-	<label for='password'>Passwort</label><br/>
-	<input type='password' name='password' id='password'/><br/>
+	<span id='emailError'></span><br />
+	<br />
+	<label for='password'>Passwort</label> <br />
+	<input type='password' name='password' id='password' /><br />
 	<span id='passwordError'></span><br />
-	<input type='submit' name='submitBtn' value='Anmelden' class='loginButton'><br/>
+	<input type='submit' name='submitBtn' value='Login' class='loginButton'><br />
 </form>
+</section>
