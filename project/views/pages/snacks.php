7c3680fe920ec463dbdbd3bae1057f5e2a9a8113
Thomas Konietzny <thomas.konietzny@fh-erfurt.de>
2021-02-12 15:19:25 +0100

    itemansicht

--- project/views/pages/snacks.php
+++ project/views/pages/snacks.php
@@ -10,7 +10,7 @@
 
     <?foreach($products as $id => $product): ?>
         <article class='product'>
-                <a href='index.php?c=pages&a=snacks&id=<?=$product->ProductID?>'>
+                <a href='index.php?c=pages&a=item&id=<?=$product->ProductID?>'>
                     <div>
                         <span><img  class='img' src='assets/pictures/products/<?=$product->ProdName;?>.png'></span><br>
                         </div>
