804087d3462853d388ecee3429dd5448ab4d8a66
Mrvn_ <marvin.hof@fh-erfurt.de>
2021-02-17 17:24:29 +0100

    changed number input

--- project/views/pages/item.php
+++ project/views/pages/item.php
@@ -1,23 +1,32 @@
 <link rel='stylesheet' type='text/css' href='assets/css/item.css'>
 
 <head>
-<title><?=isset($title)? 'SnackIt: '. $title : 'SnackIt'?></title>
+    <title><?= isset($title) ? 'SnackIt: ' . $title : 'SnackIt' ?></title>
 </head>
 
 <section class='product'>
-        <span><img  class='img' src='assets/pictures/products/<?=$product->ProdName;?>.png'></span><br>
-            <span class="description">
-                <h1><?=$product->ProdName?></h1>
-                <br>
-                Preis: <?=$product->Price?>€ <br>
-                Es sind <?=$product->OnStock?> Stück verfügbar <br>
-                <br>
-                <label>Bitte wähle die Stückzahl aus</label>
-                <form method='post' class='addToCart'>
-            <span>Anzahl </span>
-            <input type='number' name='count' class='input' value=1 min=1 max=<?=$product->OnStock?> step=1 ><br/>
-            <input type='submit' name='addToCart' class='button' value='In den Einkaufswagen' /><br />
+    <span><img class='img' src='assets/pictures/products/<?= $product->ProdName; ?>.png'></span><br>
+    <span class="description">
+        <h1><?= $product->ProdName ?></h1>
+        <br>
+        Preis: <?= $product->Price ?>€ <br>
+        Es sind <?= $product->OnStock ?> Stück verfügbar <br>
+        <br>
+        <label>Bitte wähle die Stückzahl aus</label>
+        <form method='post' class='addToCart'>
+            <br>
+            <div class="number-input">
+                <button type="button" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">-</button>
+                <input class="count" min="0" max=<?= $product->OnStock ?> name="count" value="1" type="number">
+                <button type="button" content onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus">+</button>
+            </div>
+            <br>
+            <br>
+            <input type='submit' name='addToCart' class='add' value='In den Einkaufswagen' /><br />
         </form>
-            </span>
+        <br>
+
+        <noscript>sry buttons only function if javascript is enabled</noscript>
+    </span>
 
 </section>
